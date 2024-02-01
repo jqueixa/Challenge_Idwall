dc52a5c709b736ec29cece5259e69b12126c26d0
jqueixa <142255645+jqueixa@users.noreply.github.com>
2024-02-01 15:20:20 -0300

    Add files via upload

---
+++ InterpoolController.cs
@@ -0,0 +1,66 @@
+﻿using Fiap.Api.AspNet.Repository.Context;
+using Fiap.Api.AspNet.Repository;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+[Route("api/[controller]")]
+[ApiController]
+public class InterpolController : ControllerBase
+{
+
+    private readonly InterpolRepository interpolRepository;
+
+    public InterpolController(DataBaseContext context)
+    {
+        interpolRepository = new InterpolRepository(context);
+    }
+
+    [HttpGet]
+    public ActionResult<List<InterpolModel>> Get()
+    {
+        try
+        {
+            var lista = interpolRepository.Listar();
+
+            if (lista != null)
+            {
+                return Ok(lista);
+            }
+            else
+            {
+                return NotFound("O usuário não apresenta nenhum tipo de restrição");
+            }
+
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpGet("{nome}")]
+    public ActionResult<InterpolModel> Get([FromRoute] string nome)
+    {
+        try
+        {
+            var interpolModel = interpolRepository.ConsultarPorParteNome(nome);
+
+            if (interpolModel != null)
+            {
+                return Ok(interpolModel);
+            }
+            else
+            {
+                return NotFound("O usuário não apresenta nenhum tipo de restrição");
+            }
+
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+}
+
---
+++ WantedController.cs
@@ -0,0 +1,67 @@
+﻿using Fiap.Api.AspNet.Repository.Context;
+using Fiap.Api.AspNet.Repository;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WantedController : ControllerBase
+{
+
+    private readonly WantedRepository wantedRepository;
+
+    public WantedController(DataBaseContext context)
+    {
+        wantedRepository = new WantedRepository(context);
+    }
+[HttpGet]
+    public ActionResult<List<WantedModel>> get()
+    {
+        try
+        {
+            var lista = wantedRepository.SaveApiDataToDatabaseAsync();
+
+            if (lista != null)
+            {
+                return Ok(lista);
+            }
+            else
+            {
+                return NotFound("O usuário não apresenta nenhum tipo de restrição");
+            }
+
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpGet("{nome}")]
+    public ActionResult<WantedModel> Get([FromRoute] string nome)
+    {
+        try
+        {
+            var wantedModel = wantedRepository.ConsultarPorParteNome(nome);
+
+            if (wantedModel != null)
+            {
+                return Ok(wantedModel);
+            }
+            else
+            {
+                return NotFound("O usuário não apresenta nenhum tipo de restrição");
+            }
+
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+
+}
